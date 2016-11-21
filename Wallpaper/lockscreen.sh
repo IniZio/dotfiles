8cf0b3f8c18125f66a4ccba552498597f0df124d
inizio <digit4free@gmail.com>
2016-11-21 10:37:09 +0800

    lockscreen

--- Wallpaper/lockscreen.sh
+++ Wallpaper/lockscreen.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh -e
 ICON=$HOME/Wallpaper/locked.png
 TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
 convert $TMPBG -scale 5% -scale 2000% $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
 i3lock -i $TMPBG
+# Turn the screen off after a delay.
+sleep 60; pgrep i3lock && xset dpms force off
