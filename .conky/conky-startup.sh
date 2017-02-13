537d95aad56fc7616636c9061d745ebd4fec720d
inizio <digit4free@gmail.com>
2017-02-13 09:14:05 +0800

    tweaks update

--- .conky/conky-startup.sh
+++ .conky/conky-startup.sh
@@ -2,5 +2,3 @@ sleep 20s
 killall conky
 cd "/home/iniz/.conky/gcalcli"
 conky -c "/home/iniz/.conky/gcalcli/.conkyrc" &
-cd "/home/iniz/.conky/inizio"
-conky -c "/home/iniz/.conky/inizio/.conkyrc" &
