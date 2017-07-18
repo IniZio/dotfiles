d1c1808659e3da69c99b7f21e6835260233f1259
inizio <digit4free@gmail.com>
2017-07-18 14:20:39 +0800

    untested one-command setup

--- .setup/setup.sh
+++ .setup/setup.sh
@@ -4,20 +4,27 @@ sudo apt update
 sudo apt install apt-fast
 alias apt='apt-fast'
 
-# i3
-sudo apt install i3 i3-wm i3blocks i3lock i3status
-sudo apt install gnome-settings-daemon nm-applet
-sudo apt install tlp conky guake meld light xprop xinput synclient fcitx fcitx-pinyin vim emacs lxappearance gnome-tweak-tool git
+## i3
+#sudo apt install i3 i3-wm i3blocks i3lock i3status
+#sudo apt install gnome-settings-daemon nm-applet lxappearance
+sudo apt install tlp conky guake meld light xprop xinput synclient fcitx fcitx-pinyin vim emacs gnome-tweak-tool git redshift-gtk
+
+# climate command
+cd /temp
+git clone https://github.com/adtac/climate.git
+cd climate
+sudo ./install
+sudo apt remove node
 
 # zsh
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s $(which zsh)
-# NOTE needa check if the plugins in the zshrc cfg wiil need to install yourself, also semems like instal oh-my-zsh replaces part of it?
 
 # AFTER ZSH
 # dot files (Setup SSH before this FIRST!!!)
-cd && git clone git@github.com:IniZio/dotfiles.git && mv dotfiles/*(DN) ./
+cd /temp
+git clone git@github.com:IniZio/dotfiles.git && mv dotfiles/*(DN) ./
 
 # fonts
 # font awesome: https://github.com/FortAwesome/Font-Awesome/releases
@@ -28,20 +35,29 @@ cd && git clone git@github.com:IniZio/dotfiles.git && mv dotfiles/*(DN) ./
 # Temps applet
 # https://github.com/jackd248/temps/releases/download/v0.7.0/Temps-linux-x64.zip
 
-# wall's stuff
-sudo apt install xautolock feh scrot
+## wall's stuff
+#sudo apt install xautolock feh scrot
 
 # theme
-sudo apt install rofi compton fontconfig-infinality
+#sudo apt install rofi compton fontconfig-infinality
 # Arc OSX theme: https://www.gnome-look.org/content/show.php/Arc-OSX-themes?content=175536
+# Arc Flatabulous theme
+cd /tmp
+sudo rm -rf /usr/share/themes/{Arc-Flatabulous,Arc-Flatabulous-Darker,Arc-Flatabulous-Dark}
+rm -rf ~/.local/share/themes/{Arc-Flatabulous,Arc-Flatabulous-Darker,Arc-Flatabulous-Dark}
+rm -rf ~/.themes/{Arc-Flatabulous,Arc-Flatabulous-Darker,Arc-Flatabulous-Dark}
+git clone https://github.com/andreisergiu98/arc-flatabulous-theme && cd arc-flatabulous-theme
+./autogen.sh --prefix=/usr
+sudo make install
+
 # Flat Plat
 cd /tmp
 curl -sL https://github.com/nana-4/Flat-Plat/archive/v20170307.tar.gz | tar xz
 cd Flat-Plat-20170307 && sudo ./install.sh && cd
 
 # pip-related: gcalcli
-sudo apt install pip
-sudo -H pip install gcalcli
+sudo apt install pip3
+sudo -H pip3 install gcalcli
 
 # Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -50,7 +66,7 @@ sudo apt update
 sudo apt install google-chrome-stable
 
 # Editor
-#   vs code
+#   VS Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -58,7 +74,7 @@ sudo sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode st
 sudo apt update
 sudo apt install code
 
-#   atom
+#   Atom
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt update
 sudo apt install atom
@@ -69,5 +85,10 @@ sudo add-apt-repository 'deb https://typora.io ./linux/'
 sudo apt update
 sudo apt install typora
 
-# Franz: https://github.com/KELiON/cerebro/releases/download/0.2.7/cerebro_0.2.7_amd64.deb
-# cerebro: https://github.com/KELiON/cerebro/releases/download/0.2.7/cerebro_0.2.7_amd64.deb
+# Franz
+cd /temp 
+curl -o- https://github.com/KELiON/cerebro/releases/download/0.2.7/cerebro_0.2.7_amd64.deb | sudo dpkg -i
+## Cerebro: https://github.com/KELiON/cerebro/releases/download/0.2.7/cerebro_0.2.7_amd64.deb
+
+# NVM
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
