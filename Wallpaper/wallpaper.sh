95d5f4940fad81c7d7fec477c88461271883c4b9
inizio <newman.kcchow@gmail.com>
2017-07-20 11:48:01 +0800

    use unsplash and accumulate it Changes: - curl to random filename

--- Wallpaper/wallpaper.sh
+++ Wallpaper/wallpaper.sh
@@ -5,7 +5,7 @@ picOpts="zoom"
 URL='https://source.unsplash.com/random/1920x1080'
 #URL=$(curl -s 'https://wall.alphacoders.com/api2.0/get.php?auth=cca3ef22b757d25401524545a4981c25&method=newest&height=1080&width=1920' | jq -r '.wallpapers[0].url_image') 
 
-curl -s -o $HOME/Wallpaper/wall.jpg -L https://unsplash.it/1920/1080/\?random
+curl -s -o "$HOME/Wallpaper/wallpaper-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1).jpg" -L https://unsplash.it/1920/1080/\?random
 #curl -s -o $HOME/Wallpaper/wall.jpg -L https://source.unsplash.com/random/1920x1080 
 #curl -s -o $HOME/Wallpaper/wall.jpg -L $URL
 
@@ -22,9 +22,9 @@ cp "$randWall" $HOME/Wallpaper/wall.jpg
 cp $HOME/Wallpaper/wall.jpg $HOME/Wallpaper/wallpaper.jpg
 
 #Set Wallpaper
-gsettings set org.gnome.desktop.background picture-uri '"file://'$HOME'/Wallpaper/wallpaper.jpg"'
+#gsettings set org.gnome.desktop.background picture-uri '"file://'$HOME'/Wallpaper/wallpaper.jpg"'
 
-gsettings set org.gnome.desktop.screensaver picture-uri '"file://'$HOME'/Wallpaper/wallpaper.jpg"'
+#gsettings set org.gnome.desktop.screensaver picture-uri '"file://'$HOME'/Wallpaper/wallpaper.jpg"'
 
 #feh --bg-scale "$HOME/Wallpaper/wallpaper.jpg"
 
