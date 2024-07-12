06362341d260ccc8f268ee79430c9f0f1642af9c
Newman Chow <newman@oursky.com>
2024-07-12 08:57:38 +0800

    Tabout plugin

---
+++ .config/nvim/lua/plugins/tabout.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "abecodes/tabout.nvim",
+    lazy = false,
+    config = function()
+      require("tabout").setup({
+        tabkey = "<Tab>",
+        backwards_tabkey = "<S-Tab>",
+        act_as_tab = true,
+        act_as_shift_tab = false,
+        default_tab = "<C-t>",
+        default_shift_tab = "<C-d>",
+        enable_backwards = true,
+        completion = false,
+        tabouts = {
+          { open = "'", close = "'" },
+          { open = '"', close = '"' },
+          { open = "`", close = "`" },
+          { open = "(", close = ")" },
+          { open = "[", close = "]" },
+          { open = "{", close = "}" },
+        },
+        ignore_beginning = true,
+        exclude = {},
+      })
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/nvim-cmp",
+    },
+    opt = true,
+    event = "InsertCharPre",
+    priority = 1000,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+}
