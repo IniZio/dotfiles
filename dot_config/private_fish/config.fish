bd0509f2e0ddd9095d20a0fb0ed6d892a60aaa0d
Newman Chow <newman@oursky.com>
2025-06-18 17:26:06 +0800

    Update .config/fish/config.fish Update .gitconfig

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,5 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+alias dc="docker compose"
+
 # (newman): Enable direnv
+# https://direnv.net/docs/hook.html#fish
+set -g direnv_fish_mode eval_after_arrow
 direnv hook fish | source
