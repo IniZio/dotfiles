492ab355492b4392f6d7f5076ac338d0a6405f4a
Newman Chow <newman@oursky.com>
2025-07-13 21:26:15 +0800

    Add chezmoi/.config/fish/config.fish.darwin Add chezmoi/nix/flake.lock Add chezmoi/nix/flake.nix Add chezmoi/nix/home-manager/default.nix Add chezmoi/nix/home-manager/homes/iniz-monster/default.nix

---
+++ chezmoi/nix/flake.nix
@@ -0,0 +1,79 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # For searching Nix packages faster
+    nix-search.url = "github:diamondburned/nix-search";
+
+    # flake framework, treats inputs as modules
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    # Mange home configurations
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Catppuccin theme
+    catppuccin.url = "github:catppuccin/nix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nix-search,
+      flake-parts,
+      home-manager,
+      catppuccin,
+    } @ inputs:
+     let
+      mkPkgsWithSystem =
+        system:
+        import inputs.nixpkgs {
+          inherit system;
+        };
+
+      # For creating home-manager configuration
+      mkHome = { identifier, system ? "x86_64-linux" }:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = mkPkgsWithSystem system;
+          modules = [
+            # Shared home-manager configuration
+            ./home-manager/default.nix
+            # User-specific configuration
+            ./home-manager/homes/${identifier}
+          ];
+          extraSpecialArgs = {
+            inherit inputs;
+          };
+        };
+    in
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+
+      perSystem =
+        {
+          system,
+          inputs',
+          self',
+          pkgs,
+          ...
+        }:
+        {
+          # `nix fmt` to format code
+          formatter = pkgs.nixfmt-rfc-style;
+        };
+
+      flake = {
+        homeConfigurations = {
+          "iniz@monster" = mkHome {
+            identifier = "iniz-monster";
+            system = "x86_64-linux";
+          };
+        };
+      };
+    };
+}
