e890aa11f6cdf0559abd4a32b405589615b5be0a
Newman Chow <newman@oursky.com>
2025-07-31 00:09:04 +0800

    Update nix/home-manager/homes/newman-oursky/default.nix

--- nix/home-manager/homes/newman-oursky/default.nix
+++ nix/home-manager/homes/newman-oursky/default.nix
@@ -53,4 +53,10 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true; # see note on other shells below
+    nix-direnv.enable = true;
+  };
 }
