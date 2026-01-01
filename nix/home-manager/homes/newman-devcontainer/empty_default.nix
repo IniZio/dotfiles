2eefbe26569ad7e172e2a1d6e70ec1b8598c2199
Newman Chow <newman@oursky.com>
2026-01-01 15:44:19 +0000

    Update nix/home-manager/homes/newman-devcontainer/default.nix

---
+++ nix/home-manager/homes/newman-devcontainer/empty_default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  config,
+  inputs,
+  pkgs,
+  ...
+}: {
+  home.username = "newman";
+  home.homeDirectory = "/home/newman";
+
+  home.stateVersion = "25.05";
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    nixd
+    chezmoi
+    jq
+    fzf
+    htop
+    git
+    fish
+    gnupg
+    blackbox
+    mise
+  ];
+
+  programs.home-manager.enable = true;
+}
+
