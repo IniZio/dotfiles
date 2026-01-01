5e1f046aa6b2c645c3c0f84ab77fc04a5b0099e4
Newman Chow <newman@oursky.com>
2026-01-01 15:49:03 +0000

    Update nix/home-manager/homes/newman-devcontainer/default.nix

---
+++ nix/home-manager/homes/newman-devcontainer/empty_default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  config,
+  inputs,
+  pkgs,
+  ...
+}: {
+  home.username = "newman";
+  home.homeDirectory = "/home/newman";
+
+  home.stateVersion = "25.05";
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    nixd
+    chezmoi
+    jq
+    fzf
+    htop
+    gnumake
+    git
+    fish
+    gnupg
+    blackbox
+    mise
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
