fb75d79b5324bd87e25db75d5026e5fd9e7093a9
Newman Chow <newman@oursky.com>
2026-01-03 18:08:25 +0800

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -17,6 +17,6 @@ set -e GIT_WORKTREE
 
 fenv source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" > /dev/null
 
-if command -v kiro > /dev/null
-    string match -q "$TERM_PROGRAM" "kiro" and . (kiro --locate-shell-integration-path fish)
-end
+mise activate fish | source
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
